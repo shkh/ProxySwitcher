8d9213a16f2c571528a7123e8a820da2776416ae
shkh <1113712+shkh@users.noreply.github.com>
2025-04-30 10:18:27 +0900

    refactor to use modules

--- chrome/scripts/background.js
+++ chrome/scripts/background.js
@@ -1,29 +1,5 @@
-const DIRECT_CONN = {
-  id: 'direct_connection',
-  color: '#7a7c7d',
-  name: 'Direct',
-}
-
-function setExtensionIcon() {
-  chrome.storage.local.get(['proxies', 'activeProxyId'], (result) => {
-    const proxies = result.proxies || [];
-    const activeProxy = proxies.find(p => p.id === result.activeProxyId);
-    const activeProxyColor = activeProxy ? activeProxy.color : DIRECT_CONN.color;
-    const canvas = new OffscreenCanvas(16, 16);
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = activeProxyColor;
-    ctx.beginPath();
-    ctx.arc(8, 8, 8, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.fillStyle = 'white';
-    ctx.font = 'italic bold 12px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText('P', 8, 9);
-    const imageData = ctx.getImageData(0, 0, 16, 16);
-    chrome.action.setIcon({ imageData: imageData });
-  });
-}
+import { setExtensionIcon } from './common.js';
+import { DIRECT_CONN } from './common.js';
 
 function backwardCompatibility() {
   chrome.storage.local.get(['proxies'], (result) => {
--- chrome/scripts/popup.js
+++ chrome/scripts/popup.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+import { setExtensionIcon } from '../scripts/common.js';
+import { generateColor } from '../scripts/common.js';
+import { DIRECT_CONN } from '../scripts/common.js';
+
+document.addEventListener('DOMContentLoaded', () => {
   const proxyList = document.getElementById('proxyList');
   const addProxyBtn = document.getElementById('addProxyBtn');
   const formContainer = document.getElementById('formContainer');
@@ -14,21 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const proxyHostInput = document.getElementById('proxyHost');
   const proxyPortInput = document.getElementById('proxyPort');
   const bypassListInput = document.getElementById('bypassList');
-
-  const DIRECT_CONN = {
-    id: 'direct_connection',
-    color: '#7a7c7d',
-    name: 'Direct',
-  }
-
-  function generateColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 
   let draggedItem = null;
 
@@ -55,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const [movedProxy] = proxies.splice(draggedIndex, 1);
         proxies.splice(targetIndex, 0, movedProxy);
 
-        chrome.storage.local.set({ proxies: proxies }, function () {
+        chrome.storage.local.set({ proxies: proxies }, () => {
           refreshProxyList();
         });
       });
@@ -176,27 +165,6 @@ document.addEventListener('DOMContentLoaded', function () {
       chrome.storage.local.set({ activeProxyId: DIRECT_CONN.id }, () =>{
         refreshProxyList();
       });
-    });
-  }
-
-  function setExtensionIcon() {
-    chrome.storage.local.get(['proxies', 'activeProxyId'], (result) => {
-      const proxies = result.proxies || [];
-      const activeProxy = proxies.find(p => p.id === result.activeProxyId);
-      const activeProxyColor = activeProxy ? activeProxy.color : DIRECT_CONN.color;
-      const canvas = new OffscreenCanvas(16, 16);
-      const ctx = canvas.getContext('2d');
-      ctx.fillStyle = activeProxyColor;
-      ctx.beginPath();
-      ctx.arc(8, 8, 8, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.fillStyle = 'white';
-      ctx.font = 'italic bold 12px Arial';
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillText('P', 8, 9);
-      const imageData = ctx.getImageData(0, 0, 16, 16);
-      chrome.action.setIcon({ imageData: imageData });
     });
   }
 
